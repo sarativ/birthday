d5205baacea8bd21c5f4b0a14114d8ed2550fadb
sarativ <94049698+sarativ@users.noreply.github.com>
2021-11-24 12:31:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from mypackage import myfirstmodule
+from mypackage import mysecondmodule
 
+myfirstmodule.myfirstfunction("ciao")
+mysecondmodule.mysecondfunction()
+
+# Note that imports should be all done at the beginning of the file
+# I place them in the middle to help you understand the different
+# import statements
+
+from mypackage.myfirstmodule import myfirstfunction
+myfirstfunction("ciao")
+
+from mypackage.myfirstmodule import myfirstfunction as myf
+myf("ciao")
+
+# BAD PRACTICE!
+from mypackage import *
+myfirstmodule.myfirstfunction("ciao")
